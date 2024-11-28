b8b14e977b25d27cea89c5487d8c9b3772382934
edelsonjaelta@gmail.com <you@example.com>
2024-11-28 15:48:47 -0400

    Cree el login, faltan algunos retoques todavia falta hacer todo lo otro

--- Frontend/src/componentes/Login.jsx
+++ Frontend/src/componentes/Login.jsx
@@ -1,27 +1,43 @@
 import React from 'react';
 import '../css/Login.css';
+import { MdOutlinePhoneIphone } from "react-icons/md";
+
 
 const Login = () => {
   return (
     <div className="Login">
-      <h1>lalalal</h1>
+      <div className="encabezado">
+          <h1>
+            <MdOutlinePhoneIphone className='icono'/>
+            Tecnocel
+          </h1>
+      </div>
       <div className="opciones">
         <a href="#">Sign in</a>
         <a href="#">Sign up</a>
       </div>
       <form action="" className='frm'>
         <div className="input-group">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        </div>
 
-        <div className="mantener">
-          <input type="checkbox" name="" id="" />
-          <label htmlFor="">Remenber password</label>
+          <div className="input-feild">
+            <label htmlFor="" >Username</label>
+            <input type="text" placeholder="Username" />
+          </div>
+          <div className="input-feild">
+            <label htmlFor="">Password</label>
+            <input type="password" placeholder="Username" />
+          </div>
+       
+          <div className="mantener">
+            <input type="checkbox" id="remember" className="custom-checkbox" />
+            <label htmlFor="remember">Remember password</label>
+          </div>
         </div>
         <div className="boton">
           <input type="submit" value="Login" />
         </div>
+
+        <a href="" className='olvidar'>Forgot password?</a>
       </form>
       </div>
   );
